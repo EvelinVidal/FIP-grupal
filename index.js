b192c074ec66df7b4b5d59d0ff5d97bf12cfb490
Devo <devoramaidana@tdf.edu.ar>
2022-08-11 20:45:17 -0300

    estilo al index.css

---
+++ index.js
@@ -0,0 +1,26 @@
+let ingreseNombre = document.getElementById("");
+ingreseNombre.addEventListener("");
+
+let ingreseApellido = document.getElementById("");
+ingreseApellido.addEventListener("");
+
+
+let ingreseMail = document.getElementById("");
+ingreseMail.addEventListener("");
+
+let botonEnviar = document.getElementById("");
+botonEnviar.addEventListener("");
+
+
+
+
+
+function   consulta (){
+
+alert(" el nombre tiene que tener  entre 3 y 20 caracteres")
+
+alert(" el apellido tiene que tener  entre 3 y 20 caracteres")
+alert(" Mail valido")
+alert(" La consulta no puede estar vacia")
+
+}
