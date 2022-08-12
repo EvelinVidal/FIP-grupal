3d9c0b561e2ad2f5e94cbcde5fb7f1b6954927a1
Yanel <yanelgomez@tdf.edu.ar>
2022-08-11 21:57:01 -0300

    agregué las correcciones de Juan

--- yanel/script.js
+++ yanel/script.js
@@ -1,2 +1,22 @@
 let botonEnviar = document.getElementById("botonEnviar");
-botonEnviar.addEventListener("click");
+botonEnviar.addEventListener("click", corroborarDatos);
+
+function corroborarDatos() {
+    let ingreseNombre = document.getElementById("ingreseNombre").value;
+    let ingreseApellido = document.getElementById("ingreseApellido").value;
+    let ingreseMail = document.getElementById("ingreseMail").value;
+    let ingreseConsulta = document.getElementById("ingreseConsulta").value;
+
+    if (ingreseNombre === "") {
+        alert("Debe ingresar un nombre");
+    }
+    if (ingreseApellido === "") {
+        alert("Debe ingresar un apellido");
+    }
+    if (ingreseMail === "") {
+        alert("Debe ingresar una dirección de e-mail válida");
+    }
+    if (ingreseConsulta === "") {
+        alert("Debe ingresar su consulta");
+    }
+}
